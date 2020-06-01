fbe4d9bb3456c23693bfca30cb8ce80fec81a0f9
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-05-31 23:51:52 -0300

    Update display.dart

--- lib/components/display.dart
+++ lib/components/display.dart
@@ -1,4 +1,6 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Display extends StatelessWidget {
   final String text;
@@ -10,7 +12,28 @@ class Display extends StatelessWidget {
     return Expanded(
       flex: 1,
       child: Container(
-        child: Text(text),
+        color: Color.fromRGBO(48, 48, 48, 1),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: AutoSizeText(
+                text,
+                minFontSize: 20,
+                maxFontSize: 80,
+                maxLines: 1,
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                    fontWeight: FontWeight.w100,
+                    decoration: TextDecoration.none,
+                    fontSize: 80,
+                    color: Colors.white),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
